49a40a28c7d4036962ec9d8ab9b41f8c92992c9e
Uju Okodike <okodikeuju@gmail.com>
2022-06-08 01:34:32 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,18 +10,18 @@
  * Return: void
  */
 
-void print_remaining_days(int month, int dat, int year)
+void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-		if (month > 2 && day >= 60)
+		if (month > 2 && >= 60)
 		{
 			day++;
 		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-	else 
+	else
 	{
 		if (month == 2 && day == 60)
 		{
@@ -34,4 +34,3 @@ void print_remaining_days(int month, int dat, int year)
 		}
 	}
 }
-	
