0a91741570e2e7edb69b67d0347e00c32ff2bec0
Uju Okodike <okodikeuju@gmail.com>
2022-06-10 15:41:36 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,22 +6,24 @@
  */
 void print_diagonal(int n)
 {
-	int len, space;
+	int x, y;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (len = 0; len < n; len++)
-		{
-			for (space = 0; space , len; space++)
-				_putchar(' ');
-			_putchar('\\');
-
-			if (len == n - 1)
-				continue;
-
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 
-	_putchar('\n');
-}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
+
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+}	
