eebbcaf69a799b8e3105069c10b883307c6eb347
Uju Okodike <okodikeuju@gmail.com>
2022-06-10 16:12:16 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,10 +9,7 @@ void print_diagonal(int n)
 	int x, y;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
-
 	else
 	{
 		for (x = 0; x < n; x++)
@@ -21,7 +18,6 @@ void print_diagonal(int n)
 			{
 				_putchar(32);
 			}
-
 		_putchar(92);
 		_putchar('\n');
 		}
